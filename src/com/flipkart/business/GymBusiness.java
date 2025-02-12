34cabc501d8eb116cb6647ba2121d2f27f2a82f3
namanbatheja <namanbatheja2011@gmail.com>
2025-02-12 12:40:02 +0530

    gymbusiness details

---
+++ src/com/flipkart/business/GymBusiness.java
@@ -0,0 +1,33 @@
+package com.flipkart.business;
+
+import com.flipkart.bean.GymCenter;
+
+public class GymBusiness {
+
+ private GymCenter gymCenter;
+
+ public GymBusiness(GymCenter gymCenter) {
+  this.gymCenter = gymCenter;
+ }
+
+ public String getGymName() {
+  return gymCenter.getGymName();
+ }
+
+ public String getGymCity() {
+  return gymCenter.getGymCity();
+ }
+
+ public float getGymPrice() {
+  return gymCenter.getGymPrice();
+ }
+
+ // Example function to retrieve slots
+ public int getNumberOfSlots() {
+  if (gymCenter.getSlots() != null) {
+   return gymCenter.getSlots().size();
+  } else {
+   return 0;
+  }
+ }
+}
