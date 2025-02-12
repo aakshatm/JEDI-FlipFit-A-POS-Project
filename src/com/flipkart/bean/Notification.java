3b570cb88006eccda046053ea9e9896c9c249661
aakshat.malhotra <aakshatmalhotra100@gmail.com>
2025-02-12 12:21:47 +0530

    all beans done

---
+++ src/com/flipkart/bean/Notification.java
@@ -0,0 +1,13 @@
+package com.flipkart.bean;
+
+public class Notification {
+    private int bookingId;
+
+    public int getBookingId() {
+        return bookingId;
+    }
+
+    public void setBookingId(int bookingId) {
+        this.bookingId = bookingId;
+    }
+}
---
+++ src/com/flipkart/bean/Payment.java
@@ -0,0 +1,31 @@
+package com.flipkart.bean;
+
+public class Payment {
+    private int transactionId;
+    private float amount;
+    private PaymentMode paymentMode;
+
+    public int getTransactionId() {
+        return transactionId;
+    }
+
+    public void setTransactionId(int transactionId) {
+        this.transactionId = transactionId;
+    }
+
+    public float getAmount() {
+        return amount;
+    }
+
+    public void setAmount(float amount) {
+        this.amount = amount;
+    }
+
+    public PaymentMode getPaymentMode() {
+        return paymentMode;
+    }
+
+    public void setPaymentMode(PaymentMode paymentMode) {
+        this.paymentMode = paymentMode;
+    }
+}
