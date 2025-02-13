c5065a9410fdcb554524ca15679fbb2c6783b58c
Ayush Raj <ayushraj1268@gmail.com>
2025-02-13 10:39:44 +0530

    fixed gym admin menu

--- src/com/flipkart/business/GymOwnerService.java
+++ src/com/flipkart/business/GymOwnerService.java
@@ -1,42 +1,42 @@
-package com.flipkart.business;
-
-import com.flipkart.bean.GymCenter;
-import com.flipkart.bean.GymOwner;
-
-public class GymOwnerService {
-    public void registerGym(GymOwner ownwer, GymCenter gym){
-
-    }
-
-    public void viewRegisteredGyms(GymOwner ownwer){
-
-    }
-
-    public void editGymDetails(GymOwner ownwer, int gymId){
-
-    }
-
-    public void removeGym(GymOwner ownwer, int gymId){
-
-    }
-
-    public void addSlots(GymOwner ownwer, int gymId, Slot slot){
-
-    }
-
-    public void editSlots(GymOwner ownwer, int gymId, int slotId, Slot newSlot){
-
-    }
-
-    public void removeSlot(GymOwner ownwer, int gymId, int slotId){
-
-    }
-
-    public void viewApprovedGyms(GymOwner ownwer){
-
-    }
-
-    public void viewPendingApprovals(GymOwner ownwer){
-
-    }
-}
+//package com.flipkart.business;
+//
+//import com.flipkart.bean.GymCenter;
+//import com.flipkart.bean.GymOwner;
+//
+//public class GymOwnerService {
+//    public void registerGym(GymOwner ownwer, GymCenter gym){
+//
+//    }
+//
+//    public void viewRegisteredGyms(GymOwner ownwer){
+//
+//    }
+//
+//    public void editGymDetails(GymOwner ownwer, int gymId){
+//
+//    }
+//
+//    public void removeGym(GymOwner ownwer, int gymId){
+//
+//    }
+//
+//    public void addSlots(GymOwner ownwer, int gymId, Slot slot){
+//
+//    }
+//
+//    public void editSlots(GymOwner ownwer, int gymId, int slotId, Slot newSlot){
+//
+//    }
+//
+////    public void removeSlot(GymOwner ownwer, int gymId, int slotId){
+//
+//    }
+//
+//    public void viewApprovedGyms(GymOwner ownwer){
+//
+//    }
+//
+//    public void viewPendingApprovals(GymOwner ownwer){
+//
+//    }
+//}
