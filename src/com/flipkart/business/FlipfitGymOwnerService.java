af71f5964f4e64689876d770f393dd540dc14e9f
aakshat.malhotra <aakshatmalhotra100@gmail.com>
2025-02-13 15:10:08 +0530

    spelling changed

--- src/com/flipkart/business/FlipfitGymOwnerService.java
+++ src/com/flipkart/business/FlipfitGymOwnerService.java
@@ -5,39 +5,39 @@ import com.flipkart.bean.FlipfitGymCenter;
 import com.flipkart.bean.Slot;
 
 public class FlipfitGymOwnerService {
-    public void registerGym(FlipfitGymOwner ownwer, FlipfitGymCenter gym){
+    public void registerGym(FlipfitGymOwner owner, FlipfitGymCenter gym){
 
     }
 
-    public void viewRegisteredGyms(FlipfitGymOwner ownwer){
+    public void viewRegisteredGyms(FlipfitGymOwner owner){
 
     }
 
-    public void editGymDetails(FlipfitGymOwner ownwer, int gymId){
+    public void editGymDetails(FlipfitGymOwner owner, int gymId){
 
     }
 
-    public void removeGym(FlipfitGymOwner ownwer, int gymId){
+    public void removeGym(FlipfitGymOwner owner, int gymId){
 
     }
 
-    public void addSlots(FlipfitGymOwner ownwer, int gymId, Slot slot){
+    public void addSlots(FlipfitGymOwner owner, int gymId, Slot slot){
 
     }
 
-    public void editSlots(FlipfitGymOwner ownwer, int gymId, int slotId, Slot newSlot){
+    public void editSlots(FlipfitGymOwner owner, int gymId, int slotId, Slot newSlot){
 
     }
 
-    public void removeSlot(FlipfitGymOwner ownwer, int gymId, int slotId){
+    public void removeSlot(FlipfitGymOwner owner, int gymId, int slotId){
 
     }
 
-    public void viewApprovedGyms(FlipfitGymOwner ownwer){
+    public void viewApprovedGyms(FlipfitGymOwner owner){
 
     }
 
-    public void viewPendingApprovals(FlipfitGymOwner ownwer){
+    public void viewPendingApprovals(FlipfitGymOwner owner){
 
     }
 }
