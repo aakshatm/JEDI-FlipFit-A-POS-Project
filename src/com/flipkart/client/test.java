b081c5168e0905c49c7b3b63e5a93c1e3bf7fff4
Deewanshi Bansal <thedeewanshibansal@gmail.com>
2025-02-16 17:24:27 +0530

    resolved conflicts

--- src/com/flipkart/client/test.java
+++ src/com/flipkart/client/test.java
@@ -2,19 +2,6 @@ package com.flipkart.client;
 
 public class test {
     public static void main(String[] args) {
-//        FlipFitGymCustomerMenu menu = new FlipFitGymCustomerMenu();
-//        menu.viewProfile("aakshatmalhotra100@gmail.com", "Election77@7");
-//        menu.bookGymSlot("aakshatmalhotra100@gmail.com");
-//        menu.viewAllBookings("aakshatmalhotra100@gmail.com", "Election77@7");
-//        menu.cancelBooking();
-//        menu.viewAllBookings("aakshatmalhotra100@gmail.com", "Election77@7");
-//        menu.logout();
 
-        FlipFitOwnerMenu menu = new FlipFitOwnerMenu();
-//        menu.displayGyms("rajesh.kumar@example.com");
-//        menu.addGym("rajesh.kumar@example.com");
-        menu.displayGyms("rajesh.kumar@example.com");
-//        menu.addSlots();
-//        menu.updateGym("rajesh.kumar@example.com");
     }
 }
